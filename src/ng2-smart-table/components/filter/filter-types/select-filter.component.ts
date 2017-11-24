232706dccb667679c647fd84850e2a799fb668f8
WeiLun_C <chongwl@nexstream.com.my>
2017-11-24 17:51:55 +0800

    Fix select filter does not filter on the first time

--- src/ng2-smart-table/components/filter/filter-types/select-filter.component.ts
+++ src/ng2-smart-table/components/filter/filter-types/select-filter.component.ts
@@ -35,7 +35,6 @@ export class SelectFilterComponent extends DefaultFilter implements OnInit {
       .skip(1)
       .distinctUntilChanged()
       .debounceTime(this.delay)
-      .skip(1)
       .subscribe((value: string) => this.setFilter());
   }
 }
